cd58eff13e59399f57255b06aa74460ab829a95a
gabrielcampos243 <gabrielcampos243@gmail.com>
2022-08-15 17:06:51 +0100

    rails done

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,29 @@
+class GamesController < ApplicationController
+  def new
+    @letters = 10.times.map { ('A'..'Z').to_a[rand(26)] }
+  end
+
+  def score
+    @view = ''
+    letters = params[:letters].split(' ')
+    word = params[:word]
+    if english(word) == true && letters_in_grid(word, letters) == true
+      @view = "#{word.size} points for Gryffindor"
+    elsif letters_in_grid(word, letters) == false
+      @view = 'Letters not provided'
+    else
+      @view = 'Not an English word'
+    end
+  end
+end
+
+def english(word)
+  url = "https://wagon-dictionary.herokuapp.com/#{word}"
+  hash = JSON.parse(URI.open(url).read)
+  hash['found']
+end
+
+def letters_in_grid(word, letters)
+  letter_array = word.upcase.chars
+  letter_array.all? { |letter| letter_array.count(letter) <= letters.count(letter) }
+end
